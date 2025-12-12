e4068ab4ce2b130fb2555d0da04eb05936fb3fa9
Saif Ahmed <155323501+saifahmed03@users.noreply.github.com>
2025-12-13 03:19:53 +0600

    Remove unnecessary logical lines in validators.js

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,10 +1,8 @@
 export const isEmailValid = (email) => /\S+@\S+\.\S+/.test(email);
-
 export const isPasswordStrong = (password) =>
   password.length >= 8 &&
   /[A-Z]/.test(password) &&
   /[a-z]/.test(password) &&
   /[0-9]/.test(password);
-
 export const isNotEmpty = (value) => value && value.trim() !== "";
 
